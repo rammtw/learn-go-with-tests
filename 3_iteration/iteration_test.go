0349c587eb93cfd5ecbc66889cf57424ebee3c71
Ramil <rammtw@gmail.com>
2022-04-10 10:02:20 +0300

    iteration test

---
+++ 3_iteration/iteration_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 10)
+	expected := strings.Repeat("a", 10)
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 3)
+	fmt.Println(repeated)
+	// Output: aaa
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
